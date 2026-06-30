c4dbb3996b33ebb2fe6879d6389fb631ff974dc1
lunxkoe <lunxkoe@gmail.com>
2026-06-30 17:23:17 +0900

    finish: C07/memory-v1

--- C07_Spring_Boot/memory-v1/src/test/java/memory/MemoryFinderTest.java
+++ C07_Spring_Boot/memory-v1/src/test/java/memory/MemoryFinderTest.java
@@ -1,4 +1,15 @@
-import static org.junit.jupiter.api.Assertions.*;
+package memory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MemoryFinderTest {
-  
+
+    @Test
+    void get() {
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        assertThat(memory).isNotNull();
+    }
 }
